67f294d7610d50de0d07df6f59cf6f20e430edf5
adarsh <axdrsh@gmail.com>
2025-08-07 04:09:14 +0530

    xyz

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
